57267820b6696d6a9b7651931cabf8d5bd7bb841
DonaldM <donald@wald.co.za>
2021-05-27 07:31:13 +0200

    Add project files.

---
+++ BETMart.BLL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BETMart.BLL
+{
+    public class Class1
+    {
+    }
+}
---
+++ BETMart.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BETMart.Common
+{
+    public class Class1
+    {
+    }
+}
